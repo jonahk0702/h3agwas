9aebae158e5e8020583bdaad7d10b255b1502b75
jonahk0702 <jonahkollenberg1@gmail.com>
2022-06-30 22:59:29 +0200

    july

--- beadPool.c
+++ beadPool.c
@@ -17,8 +17,7 @@
 //On first iteration, each function will return sample data. Testing my c syntax
 //On second iteration, each function will return proper data.
 
-
-struct locusEntry{
+typedef struct{
   char* ilmn_id;
   char* name;
   char* snp;
@@ -29,27 +28,28 @@ struct locusEntry{
   int address_b;
   int ref_strand;
   int source_strand;
-};
-
+} locusEntry;
 
 //char* read_ushort(char* filename);
 uint32_t read_int(FILE* ptr); // I know, read_int returns a long...
 char* read_float(char* filename);
 int read_byte(FILE* ptr);
-char* read_string(FILE* ptr);
+char*  read_string(FILE* ptr);
 char read_char(char* handle);
 int decode_code_point(char **s);
-int __parse_file(char filename[40]);
+int __parse_file(char* filename);
 
 int id2hash(char* name);
 int  hash_find(char ** bim_table, char * snp);
 void hash_add(char * snp, int p);
 void hash_init();
-struct locusEntry* create_locus_entry(FILE* ptr);
+locusEntry* create_locus_entry(FILE* ptr);
 
 //Now declaring functions from the SourceStrang object
 char to_string(int source_strand);
 int from_string(char source_strand);
+
+int ref_from_string(char ref_strand);
 
 
 
@@ -61,15 +61,14 @@ const static int  mask = hash_size-1;
 static char **id_table;
 
 
-
-
 //atrributes. The sizes of these still needs to be calculated
 //Would rather store them all on the heap for some flexibility
 int main(){
   printf("Starting...\n");
+  
   int t =  __parse_file("/home/jonahk/H3Africa.bpm");
 
-
+  
   //COMPLEMENT_MAP = {"A": "T", "T": "A", "C": "G", "G": "C", "D": "D", "I": "I"};
   //This should be a map.
   char mapKeys[7] = {'A', 'T', 'C', 'G', 'D', 'I'};
@@ -82,7 +81,7 @@ int main(){
 //Actually declaring the functions
 //I think these should be put into anther file and imported. Just figuring out the syntax
 
-int __parse_file(char filename[40]){
+int __parse_file(char* filename){
   char buffer[3];
 
   char* unstrct_string;
@@ -95,6 +94,7 @@ int __parse_file(char filename[40]){
   int versionInt;
   int readIntVer;
   uint32_t num_loci;
+
 
   ptr = fopen(filename,"rb");
   if (ptr == NULL) {
@@ -142,12 +142,13 @@ int __parse_file(char filename[40]){
   }
 
   manifest_name = read_string(ptr);
+
   manifest_name[29] = '\0';
-  printf("Manifest name read:%s\n\n", manifest_name);
+  printf("Manifest name read succesfully:%s\n\n", manifest_name);
 
   if(readIntVer > 1){
     control_config = read_string(ptr);
-    printf("control_config read\n\n");
+     printf("control_config read successfully\n\n");
   }
 
   num_loci = read_int(ptr);
@@ -155,32 +156,32 @@ int __parse_file(char filename[40]){
 
   char** names = (char **)calloc(num_loci, sizeof(char*));
 
-  char** id_table = (char **) calloc(num_loci, sizeof(char*));
-
   hash_init();
   printf("hashtable initilised\n\n");
   fseek(ptr, 4 * num_loci, SEEK_CUR);
 
+
   for(int i = 0; i < num_loci; ++i){
-    char* tempStr = read_string(ptr);
-    names[i] = (char*)malloc(strlen(tempStr) + 1);
-    strcpy(names[i], tempStr);
+    names[i] = read_string(ptr);
+      //  (char*)malloc(strlen(tempStr) + 1);
+      // strcpy(names[i], tempStr);
     //Here I want to make the Hash Table!
     // They key is tempStr. the bvalue is is i.
-    hash_add(tempStr, i);
-    id_table[i] = (char*)malloc(strlen(tempStr)+1);
-    strcpy(id_table[i], tempStr);
+    hash_add(names[i], i);
+    //Note that Id_table is the same as names. Can reuse
+    //   id_table[i] = (char*)malloc(strlen(tempStr)+1);
+    //strcpy(id_table[i], tempStr);
   }
   printf("Array of all names obtained\n\n");
   printf("Hash table created\n\n");
 
   int* normalization_id = (int*)calloc(num_loci, sizeof(uint8_t));
 
+  //Not 10 here!! NUm loci!!
   for(int i = 0; i < num_loci; ++i){
-    uint8_t temp = read_byte(ptr);
-    normalization_id[i] = temp;
+    normalization_id[i] = read_byte(ptr);
   }
-  printf("Normilizatio ID array created");
+  printf("Normilizatio ID array created\n\n");
 
   int* assay_types = (int*)calloc(num_loci, sizeof(int));
   int* addresses = (int*)calloc(num_loci, sizeof(int));
@@ -196,27 +197,33 @@ int __parse_file(char filename[40]){
 
   //for(int i = 0; i < num_loci; ++i){
   for(int i = 0; i < 1; ++i){
-    struct locusEntry *locus_entry = create_locus_entry(ptr);
+    locusEntry *locus_entry = create_locus_entry(ptr);
+    char* theName = locus_entry->name;
+    printf("theName is :%s\n\n", theName);
+    int* bb = hash_find(id_table, theName);
+    printf("the BB is: %i\n\n", bb);
+    int poss = table[bb];
+    printf("Poss is :%i\n\n", poss);
+    //      assay_types[name_lookup]
   }
 
 
 
   printf("I made it to the end.\n");
   printf("freeing memeory...\n");
-  for(int i = 0; i < num_loci; i++){
-    free(names[i]);
-  }
-  free(names);
-  free(id_table);
-  free(normalization_id);
+
+  //  free(names);
+  // free(normalization_id);
   free(assay_types);
-  free(addresses);
+  // free(addresses);
   free(snps);
   free(chroms);
   free(map_infos);
   free(ref_strands);
   free(source_strands);
+
   return 1;
+  
 }
 
 //The hash Function
@@ -241,11 +248,11 @@ int id2hash ( char * name) {
 }
 
 
-int  hash_find(char ** bim_table, char * snp) {
+int  hash_find(char ** id_table, char * snp) {
   int ind, orig,i=1;
   orig=ind = id2hash(snp);
-//  printf("has is %i\n\n", orig);
-  while((table[ind]!=-1) && (strcmp(bim_table[table[ind]],snp) != 0)) {
+
+  while((table[ind]!=-1) && (strcmp(id_table[table[ind]],snp) != 0)) {
     ind=(ind+i)&mask;
     i=i+2;
   }
@@ -284,6 +291,7 @@ uint32_t read_int(FILE* ptr){
 
    return num;
 }
+
 char* read_string(FILE* ptr){
   uint32_t total_length = 0;
 
@@ -297,6 +305,7 @@ char* read_string(FILE* ptr){
   if(partial_length < 0){
     partial_length = 256 + partial_length;
   }
+
 
   int b = partial_length & 0x80;
   while(b > 0){
@@ -312,19 +321,25 @@ char* read_string(FILE* ptr){
 
 
   total_length += partial_length << (7 * num_bytes);
+  
+  char* result;
 
-  char *result = (char*)malloc(total_length  + 1);
+  if(total_length == 0){
+    result = "";
 
+  }else{
+    result=(char*)malloc(total_length+1);
     readAttempt = fread(result, total_length, 1, ptr);
-  if(readAttempt != 1){
-    printf("Error reading in string");
-    exit(-12);
-  }
-  result[total_length] = '\0';
+    if(readAttempt != 1){
+      printf("Error reading in string");
+      exit(-12);
+    }
+    result[total_length] = '\0';
 
-  if(strlen(result) < total_length){
-    printf("Unable to read the entire string");
-    exit(-12);
+    if(strlen(result) < total_length){
+      printf("Unable to read the entire string");
+      exit(-12);
+    }
   }
 
   return result;
@@ -333,8 +348,9 @@ char* read_string(FILE* ptr){
 
 //LocusEntry object functions
 
-struct locusEntry* create_locus_entry(FILE* ptr){
-  struct locusEntry* locus_entry;
+locusEntry* create_locus_entry(FILE* ptr){
+  locusEntry* locus_entry = (locusEntry*)malloc(sizeof(locusEntry));
+  
 
   //This is initiallisation Phase
   char* ilmn_id = "";
@@ -347,91 +363,112 @@ struct locusEntry* create_locus_entry(FILE* ptr){
   int address_b = -1;
   int ref_strand = 0;
   int source_strand = 0;
-
   //Entering the pass_file phase
   int version = read_int(ptr);
   printf("The given version is:%i\n\n", version);
 
 
  if(version == 6 || version == 7 || version == 8){
-    printf("NO The IF staement does not run!!!\n\n");
     ilmn_id = read_string(ptr);
-    printf("ilm_id is %s\n\n", ilmn_id);
-    printf("test 1");
-    char ** arr = (char**)calloc(25, sizeof(char*));
-    printf("test 2");
-    char* str;
-    printf("test 3");
-    strcpy(str, ilmn_id);
-
-    char temp[30];
+    
+    char* temp = (char*)malloc(sizeof(char)*45);
+    
     int count = 0;
-    for(int i = 0; i < strlen(str); ++i){
-      printf("running on it:%i\n\n", i);
-        if(str[i] != '_'){
-            strncat(temp, &str[i],1 );
-        }else{
-            arr[count] = (char*)malloc(strlen(temp) + 1);
-            strcpy(arr[count], temp);
-            count ++;
-            strcpy(temp, "");
-        }
+    int usedCounter = 0;
+    
+    int target = -1;
+    for(int i = 0; ilmn_id[i]!=0; i++){
+      if(ilmn_id[i] == '_'){
+	target = i;
+      }
     }
-
-
-  printf("the temp is :%s\n\n", temp);
-    arr[count] = (char*)malloc(strlen(temp) + 1);
-    strcpy(arr[count], temp);
-    char value = *arr[count-1];
-
+    
+    char value = ilmn_id[target - 1];
+    
     source_strand = from_string(value);
+    
     name = read_string(ptr);
 
+    char* a;
     for(int i = 0; i < 3; ++i){
-      read_string(ptr);
+      a = read_string(ptr);
     }
+    
     int tempRead;
     int readAttempt = fread(&tempRead, 4, 1, ptr);
+    
+    
     for(int i = 0; i < 2; ++i){
-      read_string(ptr);
+      a = read_string(ptr);
     }
+
     snp = read_string(ptr);
-    printf("snp is :%s\n\n", snp);
+    
     chrom = read_string(ptr);
 
     for(int i = 0; i < 2; ++ i){
-      read_string(ptr);
+      a = read_string(ptr);
     }
 
-        map_info = atoi(read_string(ptr));
-    printf("map info:%i\n\n", map_info);
+    map_info = atoi(read_string(ptr));
 
-
-
-    for(int i = 0; i < count; ++i){
-        free(arr[i]);
+    for(int i = 0; i < 2; ++i){
+      a = read_string(ptr);
     }
-    free(arr);
+
+    address_a = read_int(ptr);
+    address_b = read_int(ptr);
+
+    for(int i = 0; i < 7; ++ i){
+      a = read_string(ptr);
+    }
+
+    readAttempt = fread(&tempRead, 3, 1, ptr);
+    assay_type = read_byte(ptr);
+    if(assay_type != 0 && assay_type != 1 && assay_type != 2){
+      printf("Format error in reading assay type from locus entry\n\n");
+      exit(15);
+
+    }
+
+    if(address_b == 0){
+      if(assay_type != 0){
+	printf("Manifest format error: Assay type is inconsistent with address B");
+	exit(15);
+      }
+    }else{
+      if(assay_type == 0){
+	printf("Manifest format error: Assay type is inconsistent with address B");
+	exit(15);
+      }
+    }
+    
+    printf("Got to the end\n\n");
   }
   if(version == 7 || version == 8){
-    //handle.read(4*4);
-  }
-  if(version == 8){
-    // self.ref_strand = RefStrand.from_string(read_string(handle))
+    int tempRead = 0;
+    int readAttempt = fread(&tempRead, 4 * 4, 1, ptr);
+ 
   }
 
+  if(version == 8){
+    ref_strand = ref_from_string(read_string(ptr)[0]);
+    printf("The ref strand is:%i\n\n", ref_strand);
+  }
+  
   locus_entry->ilmn_id = ilmn_id;
   locus_entry->name = name;
   locus_entry->snp = snp;
-
-   locus_entry->chrom = chrom;
+  
+  locus_entry->chrom = chrom;
   locus_entry->map_info = map_info;
   locus_entry->assay_type = assay_type;
   locus_entry->address_a = address_a;
   locus_entry->address_b = address_b;
   locus_entry->ref_strand = ref_strand;
   locus_entry->source_strand = 0;
-
+  
+  printf("line 437 reached\n\n");
   return locus_entry;
 }
 
@@ -440,10 +477,10 @@ char to_string(int source_strand){
   if(source_strand == 0){
     return 'U';
   }
-  if(source_strand == 1){
+  else if(source_strand == 1){
     return 'F';
   }
-  if(source_strand == 2){
+  else if(source_strand == 2){
     return 'R';
 
   }
@@ -454,18 +491,29 @@ char to_string(int source_strand){
 }
 
 int from_string(char source_strand){
-
   if(source_strand == 'U' || source_strand == '\0'){
     return 0;
   }
-  if(source_strand == 'F'){
+  else if(source_strand == 'F'){
     return 1;
   }
-  if(source_strand == 'R'){
+  else if(source_strand == 'R'){
     return 2;
   }
 
   //This should not have run
   printf("invalid source_strand given in from_string function");
   exit(-13);
+}
+
+int ref_from_string(char ref_strand){
+  if(ref_strand == 'U' || ref_strand == '\0'){
+    return 0;
+  }else if(ref_strand == '+'){
+    return 1;
+  }else if(ref_strand == '-'){
+    return 2;
+  }
+
+
 }
